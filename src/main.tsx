10abcd9236022812c8c563360d0ecff5bf6bcb52
yeol <yeol8810@gmail.com>
2022-10-22 16:08:33 +0900

    Header part Two Done

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { RecoilRoot } from 'recoil';
-import { darkTheme } from './style/theme';
 import { createGlobalStyle, ThemeProvider } from 'styled-components';
 import Jua from './fonts/Jua-Regular.ttf';
+import { theme } from './style/theme';
 
 const GlobalStyle = createGlobalStyle`
 @font-face {
@@ -16,60 +16,66 @@ a, abbr, acronym, address, big, cite, code,
 del, dfn, em, img, ins, kbd, q, s, samp,
 small, strike, strong, sub, sup, tt, var,
 b, u, i, center,
-dl, dt, dd, ol, ul, li,
+dl, dt, dd, menu, ol, ul, li,
 fieldset, form, label, legend,
 table, caption, tbody, tfoot, thead, tr, th, td,
-article, aside, canvas, details, embed, 
-figure, figcaption, footer, header, hgroup, 
-menu, nav, output, ruby, section, summary,
+article, aside, canvas, details, embed,
+figure, figcaption, footer, header, hgroup,
+main, menu, nav, output, ruby, section, summary,
 time, mark, audio, video {
-	margin: 0;
-	padding: 0;
-	border: 0;
-	font-size: 100%;
-	font: inherit;
-	vertical-align: baseline;
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-size: 100%;
+  font: inherit;
+  vertical-align: baseline;
 }
 /* HTML5 display-role reset for older browsers */
-article, aside, details, figcaption, figure, 
-footer, header, hgroup, menu, nav, section {
-	display: block;
+article, aside, details, figcaption, figure,
+footer, header, hgroup, main, menu, nav, section {
+  display: block;
+}
+/* HTML5 hidden-attribute fix for newer browsers */
+*[hidden] {
+    display: none;
 }
 body {
-	line-height: 1;
-	font-family: 'Jua';
-    background: linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238));
-
+  line-height: 1;
 }
-ol, ul {
-	list-style: none;
+menu, ol, ul {
+  list-style: none;
 }
 blockquote, q {
-	quotes: none;
+  quotes: none;
 }
 blockquote:before, blockquote:after,
 q:before, q:after {
-	content: '';
-	content: none;
+  content: '';
+  content: none;
 }
 table {
-	border-collapse: collapse;
-	border-spacing: 0;
+  border-collapse: collapse;
+  border-spacing: 0;
 }
-*{
+* {
   box-sizing: border-box;
-  font-family: 'Jua';
 }
-a{
-  text-decoration: none;
-  color: inherit;
+body {
+  font-weight: 300;
+  font-family: 'Source Sans Pro', sans-serif;
+  color:black;
+  line-height: 1.2;
+  
 }
-
+a {
+  text-decoration:none;
+  color:inherit;
+}
 `;
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <RecoilRoot>
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={theme}>
       <GlobalStyle />
       <App />
     </ThemeProvider>
